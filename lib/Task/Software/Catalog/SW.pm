d2087bbe98aaa392168ec9374616a737de6d5606
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-11-01 19:27:57 +0700

    Initial commit

---
+++ lib/Task/Software/Catalog/SW.pm
@@ -0,0 +1,21 @@
+package Task::Software::Catalog::SW;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: All Software::Catalog::SW::* modules
+
+=pkggroup Included modules
+
+=pkg Software::Catalog::SW::bulwark::qt
+
+=pkg Software::Catalog::SW::dummy::undownloadable
+
+=pkg Software::Catalog::SW::firefox
+
+=pkg Software::Catalog::SW::zcoin::qt
+
+=pkg Software::Catalog::SW::zotero
+
+=cut
